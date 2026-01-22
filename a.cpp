f9b060375410201ff849583f33050dc3b014e6ad
Ahmed Hany Fathy <158525685+Ahmedhany-1@users.noreply.github.com>
2026-01-22 20:02:45 +0200

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,13 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+
+
+int main() {
+    string s = "ssa";
+    if(s.find("World")) cout << "exists\n";
+    else cout << "not exists\n";
+     
+    return 0;
+}
