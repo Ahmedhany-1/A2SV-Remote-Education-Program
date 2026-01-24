5cca55a50973b20f9d45757a483e527d22f763eb
Ahmed Hany Fathy <158525685+Ahmedhany-1@users.noreply.github.com>
2026-01-24 03:05:30 +0200

    [2469] [Time Beats: 100.00%] [Memory Beats: 25.30%] - LeetPush

---
+++ Convert-the-Temperature.py
@@ -0,0 +1,7 @@
+1class Solution:
+2    def convertTemperature(self, celsius: float) -> List[float]:
+3        Kelvin = celsius + 273.15
+4        Fahrenheit = celsius * 1.80 + 32.00
+5
+6        return [Kelvin, Fahrenheit]
+7        
