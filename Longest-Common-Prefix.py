f17b0167681475d99bdacae6b002308ee66eaa4b
Ahmed Hany Fathy <158525685+Ahmedhany-1@users.noreply.github.com>
2026-01-26 14:06:56 +0200

    [14] [Time Beats: 100.00%] [Memory Beats: 26.51%] - LeetPush

--- Longest-Common-Prefix.py
+++ Longest-Common-Prefix.py
@@ -1,15 +1,16 @@
 1class Solution:
 2    def longestCommonPrefix(self, strs: List[str]) -> str:
 3        ans = ""
-4        first_str = strs[0]
-5        last_str = strs[-1]
-6        for i in range(min(len(first_str), len(last_str))):
-7            if(first_str[i] != last_str[i]):
-8                return ans
-9            ans += first_str[i]
-10        return ans
-11
+4        strs = sorted(strs)
+5        first_str = strs[0]
+6        last_str = strs[-1]
+7        for i in range(min(len(first_str), len(last_str))):
+8            if(first_str[i] != last_str[i]):
+9                return ans
+10            ans += first_str[i]
+11        return ans
 12
 13
 14
 15
+16
