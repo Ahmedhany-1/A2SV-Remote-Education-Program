914daed3d11e0f37685c2e85553ff67b39c85409
Ahmed Hany <ahmedfathyhany@gmail.com>
2026-02-02 19:58:04 +0200

    Refactor D_Sereja_and_Dima.py to include an alternative solution implementation

--- Contest#2/D_Sereja_and_Dima.py
+++ Contest#2/D_Sereja_and_Dima.py
@@ -28,3 +28,31 @@ while i <= j:
 
 
 print(ans_a, ans_b)
+
+"""
+n = int(input())
+a = list(map(int, input().split()))
+
+ans_a = 0
+ans_b = 0
+
+f_turn = True 
+
+for i in range(n):
+    if(a[0] > a[-1]):
+        value = a[0]
+        a.remove(a[0])
+    else:
+        value = a[-1]
+        a.remove(a[-1])
+  
+    if f_turn:
+        ans_a += value
+    else:
+        ans_b += value
+    
+    f_turn = not f_turn
+ 
+print(ans_a, ans_b)
+
+"""
