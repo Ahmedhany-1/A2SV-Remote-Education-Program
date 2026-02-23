d097a28618868acd4f783a58c4f7dcbf5d007b77
Ahmed Hany Fathy <158525685+Ahmedhany-1@users.noreply.github.com>
2026-02-23 21:56:16 +0200

    Add solution for Sorting: Bubble Sort | HackerRank (trial 1)

---
+++ hackerrank/sorting-bubble-sort-hackerrank_trial1.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def countSwaps(a):
+    n = len(a)
+    swaps = 0
+    
+    for i in range(n):
+        for j in range(n - 1):
+            # Swap adjacent elements if they are in decreasing order
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+                swaps += 1
+    
+    # Output the results
+    print(f"Array is sorted in {swaps} swaps.")
+    print(f"First Element: {a[0]}")
+    print(f"Last Element: {a[-1]}") 
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
+
+
